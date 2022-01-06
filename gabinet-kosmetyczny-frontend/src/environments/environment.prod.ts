9c5b8548c58262d67f7aae1790ab1638bae43e71
hubert.wasik <hubert.wasik@asseco.pl>
2022-01-06 14:23:12 +0100

    BPC config

--- gabinet-kosmetyczny-frontend/src/environments/environment.prod.ts
+++ gabinet-kosmetyczny-frontend/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ const config_env_hostname = "http://54.72.214.10:8083";
 
 export const environment = {
     APP_PREFIX: "HP-",
-    production: false,
+    production: true,
     api: config_env_hostname + "/api/"
 };
